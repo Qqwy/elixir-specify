5e04c013b943725da79560f689446078a26e2b24
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-16 22:34:35 +0200

    put tests in a describe block

--- test/confy_test.exs
+++ test/confy_test.exs
@@ -5,31 +5,34 @@ defmodule ConfyTest do
 
   doctest Confy
 
-  defmodule Foo do
-    require Confy
-    Confy.defconfig do
-      @doc "a name"
-      field :name, :string, default: "Jabberwocky"
-      @doc "how old this Foo is"
-      field :age, :integer
-    end
-  end
+  describe "Simple examples of Confy.defconfig/2" do
 
-  test "Basic configuration works without glaring problems" do
-    assert Confy.load(Foo, overrides: [age: 42]) == %Foo{name: "Jabberwocky", age: 42}
-    assert_raise(Confy.MissingRequiredFieldsError, fn ->
-      Foo.load()
-    end)
-  end
-
-  test "Warnings are thrown when defining a configuration with missing doc strings" do
-    assert capture_io(:stderr, fn ->
-      defmodule MissingDocs do
-        require Confy
-        Confy.defconfig do
-          field :name, default: "Slatibartfast"
-        end
+    defmodule Foo do
+      require Confy
+      Confy.defconfig do
+        @doc "a name"
+        field :name, :string, default: "Jabberwocky"
+        @doc "how old this Foo is"
+        field :age, :integer
       end
-    end) =~ "Missing documentation for configuration field `name`. Please add it by adding `@doc \"field documentation here\"` above the line where you define it."
+    end
+
+    test "Basic configuration works without glaring problems" do
+      assert Confy.load(Foo, overrides: [age: 42]) == %Foo{name: "Jabberwocky", age: 42}
+      assert_raise(Confy.MissingRequiredFieldsError, fn ->
+        Foo.load()
+      end)
+    end
+
+    test "Warnings are thrown when defining a configuration with missing doc strings" do
+      assert capture_io(:stderr, fn ->
+        defmodule MissingDocs do
+          require Confy
+          Confy.defconfig do
+            field :name, default: "Slatibartfast"
+          end
+        end
+      end) =~ "Missing documentation for configuration field `name`. Please add it by adding `@doc \"field documentation here\"` above the line where you define it."
+    end
   end
 end
