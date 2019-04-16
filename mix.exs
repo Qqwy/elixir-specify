678d0190b88550ecc192e7de144e313a5c4a82b3
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-16 22:02:02 +0200

    Setting up Travis CI

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Confy.MixProject do
     [
       app: :confy,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env),
