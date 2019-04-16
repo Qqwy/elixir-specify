7e5bac29ded4868a7d3e44e1a43251030915ec5e
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-16 22:06:57 +0200

    initial simple unit test

--- test/confy_test.exs
+++ test/confy_test.exs
@@ -2,7 +2,21 @@ defmodule ConfyTest do
   use ExUnit.Case
   doctest Confy
 
-  test "greets the world" do
-    assert Confy.hello() == :world
+  defmodule Foo do
+    require Confy
+    Confy.defconfig do
+      @doc "a name"
+      field :name, :string, default: "Jabberwocky"
+      @doc "how old this Foo is"
+      field :age, :integer
+    end
+  end
+
+  test "Basic configuration works without glaring problems" do
+
+    assert Foo.load(overrides: [age: 42]) == %Foo{name: "Jabberwocky", age: 42}
+    assert_raise(Confy.MissingRequiredFieldsError, fn ->
+      Foo.load()
+    end)
   end
 end
