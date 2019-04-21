c6c2b0b40aa84d502c7e11a3b65808c27a38c205
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-21 22:33:50 +0200

    Check if parsers are called

--- test/confy_test.exs
+++ test/confy_test.exs
@@ -1,34 +1,35 @@
 defmodule ConfyTest do
   use ExUnit.Case
+  use ExUnitProperties
   import ExUnit.CaptureIO
 
   doctest Confy
 
   describe "Simple examples of Confy.defconfig/2" do
-    defmodule Foo do
+    defmodule BasicConfyExample do
       require Confy
 
       Confy.defconfig do
         @doc "a name"
         field(:name, :string, default: "Jabberwocky")
-        @doc "how old this Foo is"
+        @doc "how old this BasicConfyExample is"
         field(:age, :integer)
       end
     end
 
     test "Basic configuration works without glaring problems" do
-      assert Confy.load(Foo, explicit_values: [age: 42]) == %Foo{name: "Jabberwocky", age: 42}
-      assert Foo.load(explicit_values: [age: 43]) == %Foo{name: "Jabberwocky", age: 43}
+      assert Confy.load(BasicConfyExample, explicit_values: [age: 42]) == %BasicConfyExample{name: "Jabberwocky", age: 42}
+      assert BasicConfyExample.load(explicit_values: [age: 43]) == %BasicConfyExample{name: "Jabberwocky", age: 43}
 
-      assert Confy.load_explicit(Foo, age: 42) == %Foo{name: "Jabberwocky", age: 42}
-      assert Foo.load_explicit(age: 44) == %Foo{name: "Jabberwocky", age: 44}
+      assert Confy.load_explicit(BasicConfyExample, age: 42) == %BasicConfyExample{name: "Jabberwocky", age: 42}
+      assert BasicConfyExample.load_explicit(age: 44) == %BasicConfyExample{name: "Jabberwocky", age: 44}
 
       assert_raise(Confy.MissingRequiredFieldsError, fn ->
-        Confy.load(Foo)
+        Confy.load(BasicConfyExample)
       end)
 
       assert_raise(Confy.MissingRequiredFieldsError, fn ->
-        Confy.load_explicit(Foo, [])
+        Confy.load_explicit(BasicConfyExample, [])
       end)
     end
 
@@ -43,6 +44,33 @@ defmodule ConfyTest do
                end
              end) =~
                "Missing documentation for configuration field `name`. Please add it by adding `@doc \"field documentation here\"` above the line where you define it."
+    end
+
+  end
+
+  describe "parsers are properly called" do
+    defmodule ParsingExample do
+      require Confy
+      Confy.defconfig do
+        @doc false
+        field :size, :integer, default: "42"
+      end
+    end
+
+    test "Parser is called with default" do
+      assert ParsingExample.load() == %ParsingExample{size: 42}
+    end
+
+    property "parser is called with value" do
+      check all thing <- term() do
+        if is_integer(thing) do
+          assert %ParsingExample{size: thing} = ParsingExample.load_explicit(size: thing)
+        else
+          assert_raise(Confy.ParsingError, fn () ->
+            ParsingExample.load_explicit(size: thing)
+          end)
+        end
+      end
     end
   end
 end
